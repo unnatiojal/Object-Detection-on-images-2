54e7fea4ac130f7d134bf40a52073bd16af7fe95
unnatiojal <85604066+unnatiojal@users.noreply.github.com>
2022-09-05 20:11:00 +0530

    Add files via upload

---
+++ device.js
@@ -0,0 +1,31 @@
+status = "";
+img = "";
+
+function preload() {
+    img = loadImage("electronic_device.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(600, 370);
+    canvas.position(370, 240);
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status: Detecting object";
+}
+
+function modelLoaded() {
+    console.log("model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+    }
+}
+
+function draw() {
+    image(img, 0, 0, 600, 370);
+}
